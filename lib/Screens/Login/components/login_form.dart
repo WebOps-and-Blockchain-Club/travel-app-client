a2550c58cb6c4d27e49eb3fea2ab5b1af6e3f9f4
hemachandra0801 <hemachandra0801@gmail.com>
2023-10-08 05:44:34 +0530

    updated frontend

---
+++ lib/Screens/Login/components/login_form.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+import '../../../components/already_have_an_account_check.dart';
+import '../../../constants.dart';
+import '../../Signup/signup_screen.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({Key? key}) : super(key: key);
+
+  @override
+  State<LoginForm> createState() => LoginFormState();
+}
+
+class LoginFormState extends State<LoginForm> {
+  bool isObscured = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            textInputAction: TextInputAction.next,
+            cursorColor: kPrimaryColor,
+            onSaved: (email) {},
+            decoration: const InputDecoration(
+              hintText: "Your email",
+              prefixIcon: Padding(
+                padding: EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.person),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+            child: TextFormField(
+              textInputAction: TextInputAction.done,
+              obscureText: isObscured,
+              cursorColor: kPrimaryColor,
+              decoration: InputDecoration(
+                hintText: "Your password",
+                prefixIcon: const Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                  child: Icon(Icons.lock),
+                ),
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isObscured = !isObscured;
+                    });
+                  },
+                  icon: Icon(
+                    isObscured
+                    ? Icons.visibility
+                    : Icons.visibility_off,
+                    color: Theme.of(context).primaryColorDark,
+                  ),
+                ),
+              ),
+              validator: (String? value) {
+                if (value == null || value.trim().isEmpty) {
+                  return "Password is required";
+                }
+                return null;
+              },
+            ),
+          ),
+          const SizedBox(height: defaultPadding),
+          Hero(
+            tag: "login_btn",
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                "Login".toUpperCase(),
+              ),
+            ),
+          ),
+          const SizedBox(height: defaultPadding),
+          AlreadyHaveAnAccountCheck(
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return const SignUpScreen();
+                  },
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
